a08bb89179cda4cf2c05a91a73d35066d179b34d
RMCordova <128437412+RMCordova@users.noreply.github.com>
2023-05-18 22:48:08 +0800

    2nd Commit - Machine problem 3

--- sysarch32-cordova/App.js
+++ sysarch32-cordova/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import { Button } from "@react-native-material/core";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <Veiw Style = {styles.container}>
+    <Button
+      title="Click Me!" onPress={() => alert("SysArch 32 Project - Cordova")}
+    /> 
+  </Veiw>
+  );cd 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = {
+container: {
+flex: 1,
+justifyContent: "center",
+alignItems: "center",
+},
+};
+
+export default App;
